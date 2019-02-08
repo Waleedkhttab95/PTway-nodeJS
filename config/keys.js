b6e773fb86aca335c1adc4e120b4d61200e264ae
waleed khaled <info@ptway.com>
2019-02-08 15:33:00 +0300

    sec2

---
+++ config/keys.js
@@ -0,0 +1,7 @@
+module.exports = {
+googleClientID: '878186063442-ng62lo9pej63lhelo27pmvmopv4b1k3d.apps.googleusercontent.com' , 
+googleClientSecret: 'DojTURxGg72hFDhVwMjKkcJR',
+mongoURI: 'mongodb://ptway_admin:waleedkh95@ds059702.mlab.com:59702/ptway-dev',
+cookieKey: 'ptwayyourway',
+jwtKey: 'ptwayyourway',
+};
