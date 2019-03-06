c025e8e7066d4547a30f69d4bf164214dae6fe68
ptway <waleed.khattab@ptway.net>
2019-03-06 14:09:18 +0300

    edit get candi

--- routes/candidate.js
+++ routes/candidate.js
@@ -1,4 +1,5 @@
 const {Candidate} = require('../models/Companies/Candidates');
+const {User} = require('../models/Users/User');
 
 module.exports = (app) =>{
  app.post('/api/postBodyC',(req,res)=>{
@@ -15,14 +16,29 @@ app.get('/api/getCandites',async (req,res)=>{
 });
 
 app.get('/api/getOneCandi',async(req,res)=>{
+  const usernames = [];
 const Bresult = await Candidate.find({'jobAd':req.query.jobAd})
 if (!Bresult) return res.status(401).send('notFound')
 const candidateNames = Bresult.map(x => x.candidateName);
 const ids = Bresult.map(x=> x._id);
-res.status(200).json({
-  candidateNames: candidateNames,
-  id: ids,
-  count : Bresult.length
-});
+
+  for(var i = 0 ; i < candidateNames.length ; i++) {
+    const users = await User.findById( candidateNames[i]).select("firstName lastName -_id")
+    console.log(users);
+    usernames.push(users);
+  }
+  
+  const username = usernames.map(x => x.firstName +' '+ x.lastName);
+
+  res.status(200).json({
+    candidateNames: candidateNames,
+    username: username, 
+    id: ids,
+    count : Bresult.length
+  });
+
+
+
+
 });
 }//endofapp
